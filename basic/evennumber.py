51d81949be2bf5920c1d018597a183b3f6737b83
Jeevitha-Sugur <jeevithapsugur@gmail.com>
2024-12-10 16:46:52 +0530

    day2 programs

---
+++ basic/evennumber.py
@@ -0,0 +1,5 @@
+input_number = 454
+# We have assumed 45 as the input number to the program
+
+if input_number % 2 == 0:
+    print(input_number, ' is an Even number')
---
+++ basic/evennumber2.py
@@ -0,0 +1,9 @@
+print('Enter a number to check if it is Even or not')
+input_number = int(input())
+#print(type(input_number))
+
+#if n%2==0:
+if input_number % 2 == 0:
+    print(input_number, ' is an Even number')
+else:
+    print(f'{input_number} is an Odd number')
